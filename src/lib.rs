de047cea3659923364081bbeb36334bfcf03c6d7
vmoiseev <vmoiseev@ro-nso.ru>
2022-03-25 20:16:28 +0700

    routes error

--- src/lib.rs
+++ src/lib.rs
@@ -34,7 +34,7 @@ impl Blog {
             App::new()
                 .data(pool.clone())
                 .wrap(middleware::Logger::default())
-                .configure(rotes::users::configure)
+                .configure(routes::users::configure)
         })
         .bind(("127.0.0.1", self.port))?
         .run()
