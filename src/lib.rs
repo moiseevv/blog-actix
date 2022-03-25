6ee96857348b9604b754caff48e23b44f879fc52
vmoiseev <vmoiseev@ro-nso.ru>
2022-03-25 20:22:44 +0700

    error mod

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,10 @@ extern crate serde_derive;
 use actix_web::{middleware, App, HttpServer};
 use diesel::prelude::*;
 use diesel::r2d2::{self, ConnectionManager};
-use crate::routes::*;
-
-
-
-
+mod errors;
+mod models;
+mod routes;
+mod schema;
 
 type Pool = r2d2::Pool<ConnectionManager<SqliteConnection>>;
 
