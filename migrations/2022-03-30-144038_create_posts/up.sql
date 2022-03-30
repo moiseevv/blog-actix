553a09dcea482aba8cf3269ffcc1d786270bb341
vmoiseev <vmoiseev@ro-nso.ru>
2022-03-30 21:54:06 +0700

    table posts

--- migrations/2022-03-30-144038_create_posts/up.sql
+++ migrations/2022-03-30-144038_create_posts/up.sql
@@ -1,1 +1,7 @@
--- Your SQL goes here
+CREATE TABLE posts(
+    id INTEGER PRIMARY KEY NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES users (id),
+    title VARCHAR NOT NULL,
+    body TEXT NOT NULL,
+    pubished BOOLEAN NOT NULL DEFAULT 0
+)
