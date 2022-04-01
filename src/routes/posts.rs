138d5384353f69d85a2b15d1b5f11c98cb7533dd
vmoiseev <vmoiseev@ro-nso.ru>
2022-04-01 19:55:27 +0700

    add posts

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -5,6 +5,15 @@ use actix_web::{web, Resource};
 use diesel::prelude::*;
 use futures::Future;
 
+pub fn configure(cfg: &mut web::ServiceConfig){
+    cfg.service(
+        web::resource("/users/{id}/posts")
+        .route(web::post().to_async(add_post))
+        .route(web::get().to_async(user_posts)),
+    )
+    .service(web::resource("/posts").route(web::get().to_async(all_posts)))
+    .service(web::resource("/posts/{id}/publish").route(web::post().to_async(publish_post)));
+}
 
 
 
@@ -34,6 +43,37 @@ fn add_post(
             let body = post.body;
             models::create_post(conn, &user, title.as_str(), body.as_str())
         })                                                                                          
+    })
+    .then(convert)
+}
+
+
+fn publish_post(
+    post_id: web::Path<i32>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError>{
+    web::block(move || {
+        let conn: &SqliteConnection = &pool.get().unwrap();
+        models::publish_post(conn, post_id.into_inner())
+    })
+    .then(convert)
+}
+
+fn user_post(
+    user_id: web::Path<i32>,
+    pool: web::Data<Pool>,
+) -> impl Future<Item = HttpResponse, Error = AppError>{
+    web::block(move||{
+        let conn: &SqliteConnection = &pool.get().unwrap();
+        models::user_post(conn, user_id.into_inner())
+    })
+    .then(convert)
+}
+
+fn all_posts(pool: web::Data<Pool>) -> impl Future<Item = HttpResponse, Error = AppError>{
+    web::block(move||{
+        let conn: &SqliteConnection = &pool.get().unwrap();
+        models::all_posts(conn)
     })
     .then(convert)
 }
