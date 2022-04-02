1cc02899364956b5c9b88facedda4108ae79ec8b
vmoiseev <vmoiseev@ro-nso.ru>
2022-04-02 18:28:20 +0700

    httpresponse

--- src/routes/posts.rs
+++ src/routes/posts.rs
@@ -1,7 +1,7 @@
 use crate::errors::AppError;
 use crate::routes::convert;
 use crate::{models, Pool};
-use actix_web::{web, Resource};
+use actix_web::{web, HttpResponse};
 use diesel::prelude::*;
 use futures::Future;
 
